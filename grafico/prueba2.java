951409373e7b0cdd6328ebfee753593a07efd3dc
AngelDCS23 <angeldcs24@gmail.com>
2023-04-15 16:40:31 +0200

    Apartado 6, subir una segunda carperta al mismo repositorio

---
+++ grafico/prueba2.java
@@ -0,0 +1,11 @@
+
+import javax.swing.JOptionPane;
+
+public class prueba2 {
+    public static void main(String[] args){
+        
+        String nombre = JOptionPane.showInputDialog("Escribe tu nombre");
+        JOptionPane.showMassege
+
+    }
+}
